81dbbb05b3cd73038f2bf9ef07c1100975bfab57
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2022-01-09 22:29:08 -0300

    Add dynamic content loading

--- scripts/home/all.js
+++ scripts/home/all.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", (e) => {
-  document.body.style.visibility = "visible";
+// Code block containing the main contents
+let code;
+// Paths to the contents of the code block
+let paths = { welcome: "welcome.html" };
+
+// Loads a file to the code block
+async function content_load(url) {
+  await fetch(url)
+    .then((response) => response.text())
+    .then((text) => {
+      code.innerHTML = text;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  code = document.querySelector("#code");
+
+  content_load(paths.welcome).then(
+    () => (document.body.style.visibility = "visible")
+  );
 });
