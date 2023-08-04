835ea00378e317e2ddc514235b4c5748efd495ba
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2023-08-04 18:10:07 -0300

    Automatically add navbar

--- src/scripts/global/navbar.ts
+++ src/scripts/global/navbar.ts
@@ -1,8 +1,8 @@
-import { trySpecify } from "@luizffgv/ts-conversions";
 import NavigationBar from "Components/NavigationBar";
 import entries from "Scripts/navbar-entries";
 
 addEventListener("DOMContentLoaded", () => {
-  const navbar = trySpecify(document.getElementById("navbar"), NavigationBar);
+  const navbar = new NavigationBar();
   navbar.addEntries(...entries);
+  document.body.insertBefore(navbar, document.body.firstChild);
 });
